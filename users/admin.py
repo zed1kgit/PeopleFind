1dfb1d600a0051d23725d1e517fb2840a9b0c322
Ilya Seleznev <zedplay.zp@gmail.com>
2025-01-06 00:33:36 +0300

    Added user to admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from users.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'role', 'pk', 'is_active')
+    list_filter = ('last_name',)
