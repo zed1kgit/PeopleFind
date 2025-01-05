7f6c6b43d050b8c6d37c5c891a6b33c61b23fb2a
Ilya Seleznev <zedplay.zp@gmail.com>
2025-01-06 00:32:24 +0300

    Created user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class UserRoles(models.TextChoices):
+    ADMIN = 'admin'
+    MODERATOR = 'moderator'
+    USER = 'user'
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='email')
+    role = models.CharField(max_length=9, choices=UserRoles.choices, default=UserRoles.USER)
+    first_name = models.CharField(max_length=60, verbose_name='First name', default='Anonymous')
+    last_name = models.CharField(max_length=60, verbose_name='Last name', default='Anonymous')
+    hobbies = models.CharField(max_length=350,verbose_name='Hobbies' , **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='Avatar', **NULLABLE)
+    is_active = models.BooleanField(default=True, verbose_name='Active')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.email}'
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+        ordering = ['id']
