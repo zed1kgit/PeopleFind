5250c4835e228ebf92185ddb406c9267924948e1
Ilya Seleznev <zedplay.zp@gmail.com>
2025-01-12 06:42:37 +0300

    Renamed project

--- PeopleFind/asgi.py
+++ PeopleFind/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for PeoplesFInd project.
+ASGI config for PeopleFind project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PeoplesFInd.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PeopleFind.settings')
 
 application = get_asgi_application()
--- PeopleFind/wsgi.py
+++ PeopleFind/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for PeoplesFInd project.
+WSGI config for PeopleFind project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PeoplesFInd.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PeopleFind.settings')
 
 application = get_wsgi_application()
