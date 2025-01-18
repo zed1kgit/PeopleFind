d892870f2d4434cf00f327cbbce1b45d7bdf6322
Ilya Seleznev <zedplay.zp@gmail.com>
2025-01-18 19:33:33 +0300

    Update users admin

--- users/admin.py
+++ users/admin.py
@@ -4,5 +4,5 @@ from users.models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'first_name', 'last_name', 'role', 'pk', 'is_active')
-    list_filter = ('last_name',)
+    list_display = ('email', 'name', 'slug', 'avatar', 'role', 'pk', 'is_active')
+    list_filter = ('name',)
