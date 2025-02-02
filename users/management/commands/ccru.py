4bbf21dd9d08ac972afd6a1b7fbac1ca6cc08729
Ilya Seleznev <zedplay.zp@gmail.com>
2025-02-02 12:28:25 +0300

    added avatars for generated users

--- users/management/commands/ccru.py
+++ users/management/commands/ccru.py
@@ -1,8 +1,10 @@
 import random
+import os
 
 from django.core.management import BaseCommand
 from django.db.models import Max
 from django.db.models.signals import post_save
+from django.conf import settings
 
 from users.models import User
 from users.signals import send_confirmation_email
@@ -23,16 +25,21 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         count = options['count']
         post_save.disconnect(send_confirmation_email, sender=User)
+        avatars_dir = os.path.join(settings.MEDIA_ROOT, 'sample')
+        avatars = [f for f in os.listdir(avatars_dir) if os.path.isfile(os.path.join(avatars_dir, f))]
         for i in range(count):
             next_pk = User.objects.all().aggregate(Max('id'))['id__max'] + 1
             if next_pk is None:
                 next_pk = 1
+            random_avatar = random.choice(avatars)
+            avatar_path = os.path.join('sample', random_avatar)
             user = User.objects.create(
                 email=f'user{next_pk}@gmail.com',
                 name=f'User{next_pk}',
                 slug=f'user{next_pk}',
                 is_active=True,
                 is_email_verified=True,
+                avatar=avatar_path,
             )
 
             interests = Interest.objects.all()
